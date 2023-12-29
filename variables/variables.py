2cdc959d99254ea3286248a07880d8c97fdef3c8
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-12-29 23:53:53 +0500

    Add print statements to variables.py

--- variables/variables.py
+++ variables/variables.py
@@ -5,3 +5,21 @@ name = "hamza"
 age = 10
 
 print(name, age)
+
+
+x = y = z = 12
+print("x:", x, "\ny:", y, "\nz:", z)
+
+x, y, z = 100, 200, 300
+print("\nx:", x, "\ny:", y, "\nz:", z)
+
+
+a = 20
+b = 30
+print("\na + b:", a + b)
+
+
+print(type(a))
+
+print(type(name))
+
