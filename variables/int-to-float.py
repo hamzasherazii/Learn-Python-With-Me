3b58fa3ce40d9ca1ec87729cd74e6aff4837751d
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-12-29 23:58:59 +0500

    converted int to float and string

---
+++ variables/int-to-float.py
@@ -0,0 +1,27 @@
+# assign value 10 to variable x
+x = 10
+
+# print value of x
+print(x)
+
+# print type of variable x, should be int
+print(type(x))
+
+# convert x to float and print its value
+print(float(x))
+
+# convert x to int and print its value
+print(int(x))
+
+# convert x to string using str() function and print its type
+print(type(str(x)))
+
+
+"""
+x = 10: This line assigns the value 10 to the variable x.
+print(x): This line prints the value of x, which is 10.
+print(type(x)): This line prints the data type of x, which is int for integer.
+print(float(x)): This line converts x to a float data type and then prints its value. In this case, it prints 10.0.
+print(int(x)): This line converts x to an int data type and then prints its value. Since x is already an integer, this just prints its value 10.
+print(type(str(x))): This line converts x to a str (string) data type and then prints its data type. The output should be <class 'str'>.
+"""
